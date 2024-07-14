f2f655c6c3b9a438ef750ad3137760ce7d0cf54d
Kaio Abreu <kaio.freires.abreu@gmail.com>
2024-07-14 02:34:06 -0300

    Bugfix CORS

--- src/main/java/one/digitalinnovation/santander_dev_week_2023/Application.java
+++ src/main/java/one/digitalinnovation/santander_dev_week_2023/Application.java
@@ -1,8 +1,11 @@
 package one.digitalinnovation.santander_dev_week_2023;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
